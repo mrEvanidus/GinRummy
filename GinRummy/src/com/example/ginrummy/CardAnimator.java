48fb54f8dd3b27162d32c7254625562f29713d30
mrEvanidus <searsj16@up.edu>
2014-10-22 16:12:28 -0700

    Card point-to-point linear animation complete

--- GinRummy/src/com/example/ginrummy/CardAnimator.java
+++ GinRummy/src/com/example/ginrummy/CardAnimator.java
@@ -1,8 +1,8 @@
 package com.example.ginrummy;
 
-import gameObjects.Card;
-import gameObjects.Rank;
-import gameObjects.Suit;
+import java.util.ArrayList;
+import gameObjects.*;
+import Animation.*;
 import android.graphics.*;
 import android.view.MotionEvent;
 
@@ -11,13 +11,20 @@ public class CardAnimator implements Animation.Animator {
 	private static final int FRAME_INTERVAL = 10;
 	private static final int BACKGROUND_COLOR = 0xff278734;
 	
+	private ArrayList<CardPath> paths;
 	protected Card card;
 	protected RectF cardPos;
 	private PointF cardDimensions;
 	
+	//constructor
 	public CardAnimator() {
 		super();
 		cardDimensions = new PointF(261, 379);
+		paths = new ArrayList<CardPath>();
+		
+		//create a new card
+		card = new Card(Rank.ACE, Suit.Spade);
+		cardPos = new RectF(0,0,0,0);
 	}
 	
 	@Override
@@ -43,6 +50,14 @@ public class CardAnimator implements Animation.Animator {
 	@Override
 	public void tick(Canvas canvas) {
 		// TODO Auto-generated method stub
+		for (CardPath path : paths)  {
+			PointF newPos = path.advance();
+			
+			int x = (int)newPos.x;
+			int y = (int)newPos.y;
+			
+			cardPos = new RectF(x,y,x + cardDimensions.x, y + cardDimensions.y);
+		}
 		
 		if (card != null && cardPos != null) card.drawOn(canvas, cardPos);
 	}
@@ -51,23 +66,16 @@ public class CardAnimator implements Animation.Animator {
 	public void onTouch(MotionEvent event) {
 		// TODO Auto-generated method stub
 		
-		int x = (int)(event.getX());
-		int y = (int)(event.getY());
+		float x = event.getX();
+		float y = event.getY();
 		
-		card = new Card(Rank.ACE, Suit.Spade);
-		cardPos = new RectF(x,y,x + cardDimensions.x, y + cardDimensions.y);
+		//teleport the card to the coordinates
+//		cardPos = new RectF(x,y,x + cardDimensions.x, y + cardDimensions.y);
+		
+		float origX = cardPos.left;
+		float origY = cardPos.top;
+		//move the card to the tapped location along a path
+		paths.add(new CardPath(card, new PointF(origX, origY), new PointF(x, y)));
+		
 	}
-	
-	/**
-	 * 
-	 * @param c
-	 * @param orgX
-	 * @param orgY
-	 * @param destX
-	 * @param destY
-	 */
-	public void slideCard(Card c, int orgX, int orgY, int destX, int destY) {
-		//calculate the distance along the path in x and y
-	}
-
 }
