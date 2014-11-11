e10af35df21471ba215b734ac5efc14ea905bbe0
mrEvanidus <searsj16@up.edu>
2014-11-10 19:08:47 -0800

    "working" alpha version. Game moves are not yet implemented.

---
+++ GinRummy/src/edu/up/cs301/ginrummy/GRComputerPlayer.java
@@ -0,0 +1,74 @@
+package edu.up.cs301.ginrummy;
+
+import java.util.Random;
+
+import android.util.Log;
+import edu.up.cs301.card.Card;
+import edu.up.cs301.card.Rank;
+import edu.up.cs301.game.GameComputerPlayer;
+import edu.up.cs301.game.infoMsg.GameInfo;
+import edu.up.cs301.game.infoMsg.TimerInfo;
+
+/**
+ * This is a computer player that slaps at an average rate given
+ * by the constructor parameter.
+ * 
+ * @author Steven R. Vegdahl
+ * @version July 2013 
+ */
+public class GRComputerPlayer extends GameComputerPlayer {	
+	public static final int THIS_PLAYER = 1;
+	// the most recent state of the game
+	private GRState savedState;
+	private Random randDeck;
+	private Random randCard;
+	
+	
+    /**
+     * Constructor for the SJComputerPlayer class; creates an "average"
+     * player.
+     * 
+     * @param name
+     * 		the player's name
+     */
+    
+    /*
+     * Constructor for the SJComputerPlayer class
+     */
+    public GRComputerPlayer(String name) {
+        // invoke superclass constructor
+        super(name);
+    }
+    /**
+     * callback method, called when we receive a message, typically from
+     * the game
+     */
+    @Override
+    protected void receiveInfo(GameInfo info) {
+    	
+    	// if we don't have a game-state, ignore
+    	if (!(info instanceof GRState)) {
+    		return;
+    	}
+    	
+    	// update our state variable
+    	savedState = (GRState)info;
+    	
+    	//Check if it's this players turn
+    	if(savedState.whoseTurn() == THIS_PLAYER){
+    		//DRAW PHASE
+    		if(savedState.getPhase() == savedState.DRAW_PHASE){
+    			// Draw a card from a random pile
+    			boolean rd = randDeck.nextBoolean();
+    			game.sendAction(new GRDrawAction(this,rd));
+    		} 
+    		//DISCARD PHASE
+    		else if (savedState.getPhase() == savedState.DISCARD_PHASE){
+    			// Discard a random card
+    			int rc = randCard.nextInt(10);
+    			Card randomCard = savedState.getHand(THIS_PLAYER).cards.get(rc);
+    			game.sendAction(new GRDiscardAction(this, randomCard));
+    		}
+    	}
+    }
+}
---
+++ GinRummy/src/edu/up/cs301/ginrummy/GRMoveAction.java
@@ -0,0 +1,54 @@
+package edu.up.cs301.ginrummy;
+
+import edu.up.cs301.game.GamePlayer;
+import edu.up.cs301.game.actionMsg.GameAction;
+
+/**
+ * A game-move object that a tic-tac-toe player sends to the game to make
+ * a move.
+ * 
+ * @author Steven R. Vegdahl
+ * @version 2 July 2001
+ */
+public abstract class GRMoveAction extends GameAction {
+	
+	private static final long serialVersionUID = -3107100271012188849L;
+
+    /**
+     * Constructor for SJMoveAction
+     *
+     * @param source the player making the move
+     * @param row the row of the square selected (0-2)
+     * @param col the column of the square selected
+     */
+    public GRMoveAction(GamePlayer player)
+    {
+        // invoke superclass constructor to set source
+        super(player);
+    }
+    
+    /**
+     * @return
+     * 		whether the move was a draw
+     */
+    public boolean isDraw() {
+    	return false;
+    }
+    
+    /**
+     * @return
+     * 		whether the move was a discard
+     */
+    public boolean isDiscard() {
+    	return false;
+    }
+    
+    /**
+     * @return
+     * 		whether the move was a knock
+     */
+    public boolean isKnock(){
+    	return false;
+    }
+
+}
